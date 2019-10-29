0cebaf1163063465aab315534b7d74f6d0307ddc
Nan Yang <nanyang24@hotmail.com>
2019-10-29 14:49:36 +0800

    feat: 修改server

--- examples/server.js
+++ examples/server.js
@@ -8,13 +8,15 @@ const WebpackConfig = require('./webpack.config')
 const app = express()
 const compiler = webpack(WebpackConfig)
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
 
 app.use(webpackHotMiddleware(compiler))
 
@@ -36,6 +38,27 @@ const router = express.Router()
 router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
+  })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function(req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
   })
 })
 

dba5f63b1725e3636e9deea97e3f5d5be4f99f80
Nan Yang <nanyang24@hotmail.com>
2019-10-29 14:49:16 +0800

    feat(xhr): 修改xhr

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 // entrance
 import { IAxiosRequestConfig } from './types/index'
+import { buildURL } from './helper/url'
+import { transformRequest } from './helper/data'
+import { processHeaders } from './helper/header'
 import xhr from './xhr'
 
-function axios(config: IAxiosRequestConfig): void {
-  xhr(config)
+const axios = (config: IAxiosRequestConfig): void => {
+  xhr(processConfig(config))
+}
+
+const processConfig = (config: IAxiosRequestConfig): IAxiosRequestConfig => {
+  config.url = transformUrl(config)
+  config.headers = transformHeader(config)
+  config.data = transformRequestData(config)
+  return config
+}
+
+const transformUrl = (config: IAxiosRequestConfig): string => {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+const transformRequestData = (config: IAxiosRequestConfig): any => transformRequest(config.data)
+
+const transformHeader = (config: IAxiosRequestConfig): any => {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,27 @@
 import { IAxiosRequestConfig } from './types/index'
 
 export default function xhr(config: IAxiosRequestConfig): void {
-  const { url, method = 'get', data = null } = config
+  const { url, method = 'get', data = null, headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
 
+  _disposeHeader(headers, data, request)
+
   request.send(data)
 }
+
+const _disposeHeader = (headers: any, data: any, request: XMLHttpRequest) => {
+  Object.keys(headers).forEach(name => {
+    // delete useless header
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      // set header
+      request.setRequestHeader(name, wipeSemicolon(headers[name]))
+    }
+  })
+}
+
+const wipeSemicolon = (value: string) => value.replace(/;$/gi, '')
